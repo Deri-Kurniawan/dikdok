22de72a38c6513d4c52a28e21b8302008aad33e9
Deri Kurniawan <deri.netuchi@gmail.com>
2022-06-21 18:01:41 +0700

    update video sources

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="app">
       <div className='app__videos'>
         <VideoCard
-          src="https://v16-webapp.tiktok.com/a345ecf41cded92f5251a3864d005df7/62b1dd20/video/tos/alisg/tos-alisg-pve-0037c001/09897ae47926476e8764b2a1e044b68d/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2198&bt=1099&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZgV2Zwe2NqVQml7Gb&mime_type=video_mp4&qs=0&rc=M2c6Ojc5ZTM5NTc0PDczM0BpanE3NGc6ZnBnPDMzODczNEA2Ni4uLWAtNTAxLi1hNjA2YSNkbGVvcjRnZTRgLS1kMS1zcw%3D%3D&l=202206210900370102440870701C0D1866"
+          src="https://v16-webapp.tiktok.com/26fa2d289bb0a225790a9a4fad1b76c0/62b1f72a/video/tos/alisg/tos-alisg-pve-0037c001/ab553a6c75734145a554257c07b1a19e/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=5902&bt=2951&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZpfSZwe2N0khml7Gb&mime_type=video_mp4&qs=0&rc=ZGY6ZjY1ODQzZDUzZ2RnOUBpM3dpbWU6ZjN3ZDMzODczNEBgL14wNi4wXzAxXjVjNTE2YSMtZWBjcjRfc2RgLS1kMS1zcw%3D%3D&l=20220621105139010245241006260A6E9D"
         />
         <VideoCard
-          src="https://v16-webapp.tiktok.com/a345ecf41cded92f5251a3864d005df7/62b1dd20/video/tos/alisg/tos-alisg-pve-0037c001/09897ae47926476e8764b2a1e044b68d/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2198&bt=1099&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZgV2Zwe2NqVQml7Gb&mime_type=video_mp4&qs=0&rc=M2c6Ojc5ZTM5NTc0PDczM0BpanE3NGc6ZnBnPDMzODczNEA2Ni4uLWAtNTAxLi1hNjA2YSNkbGVvcjRnZTRgLS1kMS1zcw%3D%3D&l=202206210900370102440870701C0D1866"
+          src="https://v16-webapp.tiktok.com/26fa2d289bb0a225790a9a4fad1b76c0/62b1f72a/video/tos/alisg/tos-alisg-pve-0037c001/ab553a6c75734145a554257c07b1a19e/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=5902&bt=2951&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZpfSZwe2N0khml7Gb&mime_type=video_mp4&qs=0&rc=ZGY6ZjY1ODQzZDUzZ2RnOUBpM3dpbWU6ZjN3ZDMzODczNEBgL14wNi4wXzAxXjVjNTE2YSMtZWBjcjRfc2RgLS1kMS1zcw%3D%3D&l=20220621105139010245241006260A6E9D"
         />
         <VideoCard
-          src="https://v16-webapp.tiktok.com/a345ecf41cded92f5251a3864d005df7/62b1dd20/video/tos/alisg/tos-alisg-pve-0037c001/09897ae47926476e8764b2a1e044b68d/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2198&bt=1099&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZgV2Zwe2NqVQml7Gb&mime_type=video_mp4&qs=0&rc=M2c6Ojc5ZTM5NTc0PDczM0BpanE3NGc6ZnBnPDMzODczNEA2Ni4uLWAtNTAxLi1hNjA2YSNkbGVvcjRnZTRgLS1kMS1zcw%3D%3D&l=202206210900370102440870701C0D1866"
+          src="https://v16-webapp.tiktok.com/26fa2d289bb0a225790a9a4fad1b76c0/62b1f72a/video/tos/alisg/tos-alisg-pve-0037c001/ab553a6c75734145a554257c07b1a19e/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=5902&bt=2951&btag=80000&cs=0&ds=3&ft=eXd.6H.EMyq8ZpfSZwe2N0khml7Gb&mime_type=video_mp4&qs=0&rc=ZGY6ZjY1ODQzZDUzZ2RnOUBpM3dpbWU6ZjN3ZDMzODczNEBgL14wNi4wXzAxXjVjNTE2YSMtZWBjcjRfc2RgLS1kMS1zcw%3D%3D&l=20220621105139010245241006260A6E9D"
         />
       </div>
     </div>
