f193e8f6e35946048f596bc63164d781ea89546e
Deri Kurniawan <deri.netuchi@gmail.com>
2022-06-22 13:11:12 +0700

    add new helper

---
+++ src/utils/abbreviateNumber.js
@@ -0,0 +1,20 @@
+const abbreviateNumber = (number, suffixSymbol) => {
+
+  // what tier? (determines SI symbol)
+  var tier = Math.log10(Math.abs(number)) / 3 | 0;
+
+  // if zero, we don't need a suffix
+  if(tier === 0) return number;
+
+  // get suffix and determine scale
+  var suffix = suffixSymbol[tier];
+  var scale = Math.pow(10, tier * 3);
+
+  // scale the number
+  var scaled = number / scale;
+
+  // format number and add suffix
+  return scaled.toFixed(1) + suffix;
+}
+
+export default abbreviateNumber
---
+++ src/utils/abbreviateNumberSymbol.js
@@ -0,0 +1,7 @@
+const INTERNATIONAL_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
+const INDONESIAN_SYMBOL = ['', 'rb', 'JT', 'M', 'T', 'KD', 'KN' ]
+
+export {
+  INTERNATIONAL_SYMBOL,
+  INDONESIAN_SYMBOL
+}
